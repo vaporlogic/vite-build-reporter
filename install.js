81806d47ed3f749ffdd11649e200b0cf453336fe
vaporlogic <vaporlogic@users.noreply.github.com>
2026-04-22 09:44:22 +0200

    feat: stable release, bundle budget support

---
+++ install.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+'use strict';
+console.log('\nvite-build-reporter installed successfully.');
+console.log('Add the plugin to your vite.config.js:');
+console.log('  import reporter from "vite-build-reporter"');
+console.log('  plugins: [reporter({ outputDir: "dist/reports" })]\n');
